24f7b178b55e91bfd65c0e2329e62b4d6dc2638f
Jack Murphy <jack@rightisleft.com>
2015-08-30 13:25:32 -0400

    Chapter 14 end

--- web/main.dart
+++ web/main.dart
@@ -3,31 +3,21 @@ import 'dart:async';
 import 'package:json_object/json_object.dart';
 
 DocumentFragment _templateFrag;
-DivElement _templateSrc;
+DivElement _frag;
 Element _view;
 
-main() {
-  print('This is main.dart from test');
-  AnchorElement btn = querySelector('#hw');
-  btn.onClick.listen(handleClick);
-
+void main() {
   // Select target where all deals will be added
   _view = querySelector('#deals');
 
   //Parse Box Template And Store Locally
   _templateFrag = querySelector('template').content;
-  _templateSrc = new Element.div();
-  _templateSrc.nodes.addAll(_templateFrag.nodes);
 
-//  render();
+  render();
 }
 
-handleClick(Event e) {
-  InputElement input = querySelector('input');
-  input.value += e.target.text + " ";
-}
 
-void render() async {
+Future render() async {
   String result = await HttpRequest.getString('deals.json');
   JsonObject response = new JsonObject.fromJsonString(result);
   List dealVOs = response.deals;
@@ -39,6 +29,7 @@ void render() async {
       aDeal.price.text = dealVO.price;
       aDeal.description.text = dealVO.description;
       aDeal.image.src = dealVO.image;
+      aDeal.button.href = dealVO.url;
 
       _view.children.add(aDeal.element);
   });
@@ -54,7 +45,8 @@ class Deal {
   DivElement element;
 
   Deal() {
-    element = _templateSrc.clone(true);
+    element = new Element.div();
+    element.nodes.add( _templateFrag.clone(true) );
     city = element.querySelector('h3');
     date = element.querySelector('h4');
     price = element.querySelector('h5');
