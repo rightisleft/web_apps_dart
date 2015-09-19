9be1b5db1ef901f598027166d16a628e82bb6d87
Jack Murphy <jackmurphy@Jacks-MacBook-Pro-2.local>
2015-09-19 19:22:12 -0400

    rely on shared data

--- lib/client/components/flight_display/flight_display.component.dart
+++ lib/client/components/flight_display/flight_display.component.dart
@@ -1,39 +1,23 @@
 part of ticket_client;
 
 @Component(
-    selector: 'flight-display',
-    properties: const ['emitter']
+    selector: 'flight-display'
 )
 @View(
     styleUrls: const ["package:tickets/client/components/flight_display/flight_display.css"],
     templateUrl: "package:tickets/client/components/flight_display/flight_display.html",
     directives: const[CORE_DIRECTIVES]
-//  encapsulation: ViewEncapsulation.NONE // Todo Jack – Google Bug
 )
 class FlightDisplay extends Object {
 
   RouteParams routeParams;
   Router router;
-
-  NgForm flight_display_form;
   num service_level;
-
   FlightQueryService queryService;
   FlightFormatter params;
-
   List<TimeDTO> flight_times;
   List<RouteDTO> routes;
-
   TimeDTO selectedDTO;
-
-  void set emitter(val) {
-    external_emitter = val;
-    external_emitter.listen(fetchData);
-  }
-
-  EventEmitter get emitter{
-    return external_emitter;
-  }
 
   FlightDisplay(Router this.router, RouteParams this.routeParams, FlightQueryService this.queryService) {
     if(routeParams.params != null && routeParams.params.isEmpty == false)
@@ -66,7 +50,6 @@ class FlightDisplay extends Object {
   {
     var post = params.toPostable();
     post['id'] = time.flight;
-
     post['level'] = service_level;
     Instruction _navigationInstruction = router.generate(['/order', post]);
     router.navigateInstruction(_navigationInstruction);
--- lib/client/components/order/recap.component.dart
+++ lib/client/components/order/recap.component.dart
@@ -11,21 +11,12 @@ part of ticket_client;
 class Recap extends Object {
   Router _router;
   RouteParams _routeParams;
-  FlightQueryService queryService;
-  FlightFormatter flightDTO;
 
-  TimeDTO timeDTO;
+  FlightFormatter fFlight;
   SharedData shared;
 
-  Recap(Router this._router, RouteParams this._routeParams,
-      FlightQueryService this.queryService, SharedData this.shared)
+  Recap(Router this._router, RouteParams this._routeParams, SharedData this.shared)
   {
-    flightDTO = new FlightFormatter.FromPost(_routeParams.params);
+    fFlight = new FlightFormatter.FromPost(_routeParams.params);
   }
-
-  String format(DateTime value)
-  {
-    return flightDTO.format(value);
-  }
-
 }
